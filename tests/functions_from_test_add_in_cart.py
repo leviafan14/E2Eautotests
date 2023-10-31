af26e998661780356f8c243fedd7f56be389c0b6
leviafan14 <14leviafan88@gmail.com>
2023-10-31 23:25:50 +0600

    Функция проверки товаров в корзине, очистка корзины, функции вынесены в отдельный файл

---
+++ tests/functions_from_test_add_in_cart.py
@@ -0,0 +1,46 @@
+import pytest
+import time
+import pytest_playwright
+from playwright.sync_api import Playwright, Page, expect
+
+# Ссылка на страницу категории товаров
+category_url = "https://dev.domka.shop/partners/supermarket/"
+# Ссылка на сайт сервиса
+shop_url = "https://dev.domka.shop/"
+profile_link = "https://dev.domka.shop/profile"
+cart_link = "https://dev.domka.shop/cart"
+customer_phone = "77853000000"
+customer_code = "1111"
+auth_state = "yes"
+
+
+# Функция очистки корзины
+def flush_cart(page: Page) -> None:
+    page.goto(cart_link)
+    try:
+        page.get_by_text("Очистить корзину").click()
+        page.get_by_role("button", name="УДАЛИТЬ И ПРОДОЛЖИТЬ").click()
+    except Exception as e:
+        print("\nКорзина пуста")
+
+
+def auth_in_shop(page: Page) -> None:
+    page.goto(profile_link)
+    # Ввод телефона покупателя
+    page.get_by_role("textbox").click()
+    page.get_by_role("textbox").fill(customer_phone)
+    page.get_by_role("textbox").click()
+    # Нажатие на кнопку получения смс-кода
+    page.get_by_role("button", name="ПОЛУЧИТЬ КОД").click()
+    # Ввод полученного кода
+    page.get_by_role("textbox").click()
+    page.get_by_role("textbox").fill(customer_code)
+    # Нажатие на кнопку отправки смс-кода
+    page.get_by_role("button", name="ОТПРАВИТЬ КОД").click()
+    # Переход в профиль
+    page.get_by_role("link", name="Профиль").click()
+    # Проверка, что пользователь авторизван в магазине
+    profile_header = page.get_by_role("heading", name="Мой профиль").text_content()
+    assert profile_header == "Мой профиль", "Не удалось авторизоваться"
+    time.sleep(1)
+
--- tests/test_add_in_cart.py
+++ tests/test_add_in_cart.py
@@ -1,17 +1,4 @@
-import pytest
-import time
-import pytest_playwright
-from playwright.sync_api import Playwright, Page, expect
-
-
-# Ссылка на страницу категории товаров
-category_url = "https://dev.domka.shop/partners/supermarket/"
-# Ссылка на сайт сервиса
-shop_url = "https://dev.domka.shop/"
-profile_link = "https://dev.domka.shop/profile"
-customer_phone = "77853000000"
-customer_code = "1111"
-auth_state = "yes"
+from functions_from_test_add_in_cart import *
 
 
 @pytest.fixture
@@ -22,28 +9,7 @@ def page(playwright: Playwright):
     page = context.new_page()
     # Если нужна авторизация в магазине
     if auth_state == "yes":
-        page.goto(profile_link)
-        # Ввод телефона покупателя
-        page.get_by_role("textbox").click()
-        page.get_by_role("textbox").fill(customer_phone)
-        page.get_by_role("textbox").click()
-        # Нажатие на кнопку получения смс-кода
-        page.get_by_role("button", name="ПОЛУЧИТЬ КОД").click()
-        # Ввод полученного кода
-        page.get_by_role("textbox").click()
-        page.get_by_role("textbox").fill(customer_code)
-        # Нажатие на кнопку отправки смс-кода
-        page.get_by_role("button", name="ОТПРАВИТЬ КОД").click()
-        # Переход в профиль
-        page.get_by_role("link", name="Профиль").click()
-        # Проверка, что пользователь авторизван в магазине
-        try:
-            page.get_by_role("heading", name = "Мой профиль")
-        except Exception as e:
-            print("Авторизация не удалась")
-            exit()
-        time.sleep(1)
-
+        auth_in_shop(page)
     else:
         pass
     return page
@@ -51,17 +17,18 @@ def page(playwright: Playwright):
 
 # Добавление товаров в корзину
 def test_add_prdoucts(page) -> None:
+    flush_cart(page)
     # Открытие списка партнеров
     page.goto(category_url)
     # Открытие страницы партнера
     page.locator(".css-qbyb0p").nth(6).click()
     # Добавление указанного количества товаров в корзину
-    for i in range(14):
+    for i in range(13):
         try:
             page.get_by_role("button", name="В КОРЗИНУ").nth(i).click()
             print(f"Итерация {i} товар добавлен")
         except Exception as e:
-            print(" Не удалось добавить товаро в корзину, на итерации {i} /n {e}")
+            print(f" Не удалось добавить товар в корзину, на итерации {i} \n {e}")
             page.get_by_role("link", name="Корзина").first.click()
     # Переход в корзину
     page.get_by_role("link", name="Корзина").first.click()

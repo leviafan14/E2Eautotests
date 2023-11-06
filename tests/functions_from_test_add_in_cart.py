1d8f2c8debc7ad2770694b79d874a348401b887a
leviafan14 <14leviafan88@gmail.com>
2023-11-06 22:43:09 +0600

    Рефакторинг теста

--- tests/functions_from_test_add_in_cart.py
+++ tests/functions_from_test_add_in_cart.py
@@ -1,7 +1,8 @@
 import pytest
 import time
 import pytest_playwright
-from playwright.sync_api import Playwright, Page, expect
+from playwright.sync_api import Playwright, Page, sync_playwright
+
 
 # Ссылка на страницу категории товаров
 shop = "https://dev.domka.shop/partners/supermarket/"
@@ -11,7 +12,7 @@ partner = 1
 shop_url = "https://dev.domka.shop/"
 profile_link = "https://dev.domka.shop/profile"
 cart_link = "https://dev.domka.shop/cart"
-customer_phone = "8530099789"
+customer_phone = "8530095789"
 customer_code = "1111"
 auth_state = "yes"
 
@@ -47,3 +48,46 @@ def auth_in_shop(page: Page) -> None:
     assert profile_header == "Мой профиль", "Не удалось авторизоваться"
     time.sleep(1)
 
+
+def add_in_cart() -> int:    
+    with sync_playwright() as playwright:
+        test_iter = 0
+        while True:
+            test_iter += 1
+            # Открытие окна бразуера
+            browser = playwright.chromium.launch(headless=False, args=["--start-maximized"])
+            context = browser.new_context(no_viewport=True)
+            page = context.new_page()
+            # Если нужна авторизация в магазине
+            if auth_state == "yes":
+                auth_in_shop(page)
+            else:
+                pass
+            flush_cart(page)
+            context.tracing.start(screenshots=True, snapshots=True, sources=True)
+            # Открытие списка партнеров
+            page.goto(shop)
+            # Открытие страницы партнера
+            page.locator(".css-qbyb0p").nth(6).click()
+            # Добавление указанного количества товаров в корзину
+            for i in range(11):
+                try:
+                    page.get_by_role("button", name="В КОРЗИНУ").nth(i).click()
+                except Exception as e:
+                    print(f" Не удалось добавить товар в корзину, на итерации {i} \n {e}")
+                    page.get_by_role("link", name="Корзина").first.click()
+            # Переход в корзину
+            page.get_by_role("link", name="Корзина").first.click()
+            time.sleep(1)
+            # Получение количества добавленных в корзину товаров
+            count_added_product = page.get_by_role("button", name="Удалить").count()
+            print(f"\nКоличество добавленных в корзину товаров {count_added_product}")
+            try:
+                page.get_by_text("Очистить корзину").click()
+            except Exception as e:
+                context.tracing.stop(path=f"context_trace_{test_iter}.zip")
+                print("\nВ корзине не отображаются товары")
+                break
+            time.sleep(1)
+            page.close()
+        return count_added_product
--- tests/test_add_in_cart.py
+++ tests/test_add_in_cart.py
@@ -1,58 +1,7 @@
 from functions_from_test_add_in_cart import *
 
 
-#@pytest.fixture
-def page(playwright: Playwright):
-    # Открытие окна бразуера
-    browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context()
-    page = context.new_page()
-    # Если нужна авторизация в магазине
-    if auth_state == "yes":
-        auth_in_shop(page)
-    else:
-        pass
-    return page
-
-
 # Добавление товаров в корзину
-def test_add_prdoucts(playwright: Playwright) -> None:
-    test_iter = 0
-    while True:
-        test_iter += 1
-        # Открытие окна бразуера
-        browser = playwright.chromium.launch(headless=False, args=["--start-maximized"])
-        context = browser.new_context(no_viewport=True)
-        page = context.new_page()
-        # Если нужна авторизация в магазине
-        if auth_state == "yes":
-            auth_in_shop(page)
-        else:
-            pass
-        flush_cart(page)
-        context.tracing.start(screenshots=True, snapshots=True, sources=True)
-        # Открытие списка партнеров
-        page.goto(shop)
-        # Открытие страницы партнера
-        page.locator(".css-qbyb0p").nth(6).click()
-        # Добавление указанного количества товаров в корзину
-        for i in range(11):
-            try:
-                page.get_by_role("button", name="В КОРЗИНУ").nth(i).click()
-            except Exception as e:
-                print(f" Не удалось добавить товар в корзину, на итерации {i} \n {e}")
-                page.get_by_role("link", name="Корзина").first.click()
-        # Переход в корзину
-        page.get_by_role("link", name="Корзина").first.click()
-        time.sleep(1)
-        count_added_product = page.get_by_role("button", name="Удалить").count()
-        print(f"\nКоличество добавленных в корзину товаров {count_added_product}")
-        try:
-            page.get_by_text("Очистить корзину").click()
-        except Exception as e:
-            context.tracing.stop(path=f"context_trace_{test_iter}.zip")
-            print("\nВ корзине не отображаются товары")
-        # Получение количества добавленных в корзину товаров
-        time.sleep(1)
-        assert count_added_product >= 1, "\nТовары в корзину не добавлены"
-        page.close()
+def test_add_prdoucts() -> None:
+    count_added_product = add_in_cart()
+    assert count_added_product >= 1, "\nТовары в корзину не добавлены"
